2dace3d7ecb6ad1ef854d8f91605e7eab9b3f674
Dhany <dhany.aritonang@koinworks.com>
2022-05-23 22:02:37 +0700

    create employee, get employee by id, update employe

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"time"
 
@@ -11,7 +12,7 @@ import (
 const (
 	DB_HOST     = "localhost"
 	DB_PORT     = "5432"
-	DB_USER     = "postgres"
+	DB_USER     = "koinworks"
 	DB_PASSWORD = ""
 	DB_NAME     = "db_go_sql"
 )
@@ -24,16 +25,60 @@ func main() {
 	}
 
 	fmt.Println("DB CONNECTED")
+	fmt.Println()
 
+	// create employee
+	emp := Employee{
+		Email:    "andi@koinworks.com",
+		Fullname: "andi",
+		Age:      30,
+		Division: "Marketing",
+	}
+	err = createEmployee(db, &emp)
+	if err != nil {
+		fmt.Println("error :", err.Error())
+		return
+	}
+
+	// get all employees
 	employees, err := getAllEmployees(db)
 
 	if err != nil {
-		panic(err)
+		fmt.Println("error: ", err.Error())
+		return
 	}
 
 	for _, employee := range *employees {
 		employee.Print()
 	}
+
+	// get employee by id
+	employeeID := 1
+	fmt.Println("\nGet Employee ID", employeeID)
+	employee, err := getEmployeeByID(db, employeeID)
+
+	if err != nil {
+		fmt.Println("error: ", err.Error())
+		return
+	}
+	employee.Print()
+
+	// update employee by id
+	employeeID = 1
+	requestUpdateEmployee := Employee{
+		Email:    "abdul.update@koinworks.com",
+		Fullname: "Abdul update",
+		Age:      31,
+		Division: "Marketing",
+	}
+	err = updateEmployeeByID(db, employeeID, &requestUpdateEmployee)
+	if err != nil {
+		fmt.Println("error: ", err.Error())
+		return
+	}
+	fmt.Println("\nUpdated Employee id", employeeID)
+	newEmployee, _ := getEmployeeByID(db, employeeID)
+	newEmployee.Print()
 }
 
 func connectDB() (*sql.DB, error) {
@@ -120,4 +165,112 @@ func getAllEmployees(db *sql.DB) (*[]Employee, error) {
 		employees = append(employees, employee)
 	}
 	return &employees, nil
+}
+
+func createEmployee(db *sql.DB, request *Employee) error {
+	query := `
+		INSERT INTO employees(full_name, email, age, division)
+		VALUES($1, $2, $3, $4)
+	`
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(request.Fullname, request.Email, request.Age, request.Division)
+
+	if err != nil {
+		if err := tx.Rollback(); err != nil {
+			return err
+		}
+
+		return err
+	}
+
+	return tx.Commit()
+}
+
+func getEmployeeByID(db *sql.DB, id int) (*Employee, error) {
+	query := `
+		SELECT
+			id,
+			full_name,
+			email,
+			age,
+			division
+		FROM employees
+		WHERE id=$1
+	`
+
+	smt, err := db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	defer smt.Close()
+
+	employee := Employee{}
+
+	row := smt.QueryRow(id)
+
+	err = row.Scan(
+		&employee.ID,
+		&employee.Fullname,
+		&employee.Email,
+		&employee.Age,
+		&employee.Division,
+	)
+
+	if err != nil {
+		tempError := fmt.Sprintf("employee with id %d not found", id)
+		return nil, errors.New(tempError)
+	}
+
+	return &employee, nil
+}
+
+func updateEmployeeByID(db *sql.DB, id int, request *Employee) error {
+	_, err := getEmployeeByID(db, id)
+	if err != nil {
+		return err
+	}
+
+	query := `
+		UPDATE employees
+		SET full_name=$1, email=$2, age=$3, division=$4
+		WHERE id=$5
+	`
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(
+		request.Fullname,
+		request.Email,
+		request.Age,
+		request.Division,
+		id,
+	)
+
+	if err != nil {
+		if err := tx.Rollback(); err != nil {
+			return err
+		}
+
+		return err
+	}
+
+	return tx.Commit()
 }
